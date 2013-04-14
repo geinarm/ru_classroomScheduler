990cd6cd35c8cee3f068e9f8478285bcb9247095
Kristjan Broder Lund <kristjan.1234@gmail.com>
2013-04-14 23:51:46 +0000

    Output result

--- src/Solver.java
+++ src/Solver.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
@@ -68,13 +71,16 @@ public class Solver {
 		SearchNode root = new SearchNode(null, 0);
 		frontier.add(root);
 		
+		SearchNode bestNode = null;
+		int foundLeafs = 0;
+		
 		//Run the search
 		while(!frontier.isEmpty())
 		{
 			//Get current node
 			SearchNode node = frontier.peek();
 			
-			System.out.println("Node: " + node.c + "-" + node.t + " score: " + node.score); // + " c: " + frontier.size()
+			//System.out.println("Node: " + node.c + "-" + node.t + " score: " + node.score); // + " c: " + frontier.size()
 			
 			//Expand current node
 			SearchNode next = expandNode(node);
@@ -107,16 +113,49 @@ public class Solver {
 			//is this a goal
 			if(next.depth == classCount){
 				System.out.println("Found a leaf " + next.score);
+				
+				if (bestNode == null || next.score > bestNode.score) {
+					bestNode = next;
+					System.out.println("It's SO much better now.");
+					
+					Class[][][] schedule = getSchedule(bestNode);
+					System.out.println("Best score: " + bestNode.score + " - Final evaluation: " + finalEvaluation(schedule));
+					System.out.println("Solved in " + (System.currentTimeMillis()-timestamp)/1000.0 + " Seconds");
+				}
+				
+//				if (++foundLeafs >= 100)
+//					break;
+			}
+			
+			// Milliseconds
+			if ((System.currentTimeMillis()-timestamp) > 500000) {
+				System.out.println("Time limit reached.");
+				break;
 			}
 				
-			System.out.println("Next: " + next.c + "-" + next.t + "  Score: " + next.score + "  Depth: " + next.depth);
+			//System.out.println("Next: " + next.c + "-" + next.t + "  Score: " + next.score + "  Depth: " + next.depth);
+			//break;
 			
 			if(next.score > 0)
 				frontier.add(next);
 		}
 		
+		Class[][][] schedule = getSchedule(bestNode);
+		
+		System.out.println("Best score: " + bestNode.score + " - Final evaluation: " + finalEvaluation(schedule));
+		printSchedule(schedule);
+		
 		System.out.println("Done");
 		System.out.println("Solved in " + (System.currentTimeMillis()-timestamp)/1000.0 + " Seconds");
+	}
+	
+	Class[][][] getSchedule(SearchNode node) {
+		Class[][][] schedule = new Class[s.rooms.size()][numTime][5];
+		for (int i = 0; i < node.assignments.length; i++) {
+			Timeslot timeslot = timeslots.get(node.assignments[i] - 1);
+			schedule[timeslot.room.roominputindex][timeslot.time][timeslot.day] = s.classes.get(i);
+		}
+		return schedule;
 	}
 	
 	public SearchNode expandNode(SearchNode node)
@@ -216,5 +255,71 @@ public class Solver {
 			}
 		}
 
+	}
+
+	private void printSchedule(Class schedule[][][]) {
+		String content ="";
+		String times[] = { "08:30", "09:20", "10:20", "11:10", "12:20", "13:10",
+				"14:00", "14:55" };
+		String days[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+		
+		for(int d=0; d < 5; d++){
+			content = content + "\n" + days[d] + ":"+"\n";
+			content = content +"---------------------------------\n";
+			
+			for(int r=0; r < s.rooms.size(); r++){
+				Room room = s.rooms.get(r);
+				content = content + (room.name + ":");
+				
+				for(int t=0; t < numTime; t++){
+					Class c = schedule[r][t][d];
+					content = content + (times[t] + " : ");
+					if(c != null)
+						content = content + (c.name + "\n");
+					else
+						content = content +("N/A\n");
+					
+				}
+			}
+		}
+		try{
+			File file = new File("output.txt");
+			 
+			// if file does not exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private int finalEvaluation(Class schedule[][][])
+	{
+		int collisions = 0;
+		
+		for(int d=0; d < 5; d++){
+			for(int t=0; t < numTime; t++){
+				for(int r=0; r < s.rooms.size(); r++){
+					if(schedule[r][t][d] != null){
+						Class c1 = schedule[r][t][d];
+						for(int c=0; c < s.rooms.size(); c++){
+							if(schedule[c][t][d] != null && c != r){
+								Class c2 = schedule[c][t][d];
+								collisions += overlapGraph[c1.inputId][c2.inputId];
+							}
+						}					
+					}
+				}
+			}
+		}
+		collisions = collisions/4;
+		return collisions;
 	}
 }
